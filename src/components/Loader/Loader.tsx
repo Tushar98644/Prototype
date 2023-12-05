d1bc0c3969d1a4149ea1ac9ef9c2bfe999800266
Tushar Banik <b.toshar@iitg.ac.in>
2023-12-05 15:00:20 +0530

    carousel

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,12 @@
+'use client'
+import { Html } from "@react-three/drei";
+
+const Loader = () => {
+    return ( 
+        <Html>
+            loader
+        </Html>
+     );
+}
+ 
+export default Loader;
